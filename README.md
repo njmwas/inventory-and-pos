32f7e15d911bd9c51ab45f6dbb3d2bb97597617f
Julius <njmwas@gmail.com>
2025-03-21 18:13:12 +0300

    Initial code

---
+++ README.md
@@ -0,0 +1,26 @@
+# Invetory and POS
+This is a demo project for an invetory application with a Point-Of-Sale.
+Its built purely with HTML Bootsrap and JavaScript
+
+## Technologies used
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
+- Bootstrap Icons
+- json-server (for backend and database)
+
+## Setting Up
+- Setup bootstrap, bootstrap-icons and json-server run 
+```
+npm install
+```
+- create db.json file outside the project folder and run json-server
+```
+json-server --watch db.json --port 3006
+```
+- Clone the repository and run it using live-server or use the following python command inside the repository folder 
+```
+python -m http.server 
+```
+
---
+++ index.js
@@ -0,0 +1,123 @@
+const enventoryListTable = document.querySelector("#enventory-list tbody");
+const posPorductsDiv = document.querySelector('#pos-products');
+
+function invetoryEnlist(product) {
+
+    const row = document.createElement('tr');
+    const col1 = document.createElement('td');
+    col1.appendChild(document.createTextNode(product.name));
+
+    const col2 = document.createElement('td');
+    col2.appendChild(document.createTextNode(product.instock));
+
+    const col3 = document.createElement('td');
+    col3.appendChild(document.createTextNode(product.price));
+
+    row.append(col1, col2, col3);
+    enventoryListTable.appendChild(row);
+
+    // enventoryListTable.innerHTML += `
+    //     <tr>
+    //         <td>${product.name}</td>
+    //         <td>${product.instock}</td>
+    //         <td>${product.price}</td>
+    //     </tr>
+    // `
+}
+
+function posEnlist(product) {
+    posPorductsDiv.innerHTML += `
+        <div class="card" style="width: 20%;">
+            <img src="images/placeholder.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h3>${product.name}</h3>
+                <p class="card-text">${product.description}</p>
+                <div class="d-flex justify-content-between">
+                <button type="button" class="btn"> <i class="bi bi-cart-plus"></i> Add to cart</button>
+                    <strong>${product.price}</strong>
+                <div>
+            </div>
+        </div>`;
+}
+
+function enlistAllProducts() {
+    fetch("http://localhost:3006/products")
+        .then(response => response.json())
+        .then(products => {
+            // console.log(products)
+            products.forEach(product => invetoryEnlist(product))
+        })
+
+}
+enlistAllProducts();
+
+document.querySelector("#invetory-form").addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const form = evt.target;
+    const productData = {
+        name: form.name.value,
+        instock: form.instock.value,
+        description: form.description.value,
+        price: form.price.value
+    }
+
+    fetch("http://localhost:3006/products", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(productData)
+    })
+        .then((response) => {
+            return response.json()
+        })
+        .then(responseBody => {
+            // console.log(responseBody);
+            invetoryEnlist(responseBody)
+            form.reset()
+        })
+
+    console.log(productData)
+});
+
+const shoppingCart = {
+    totalCost: 0,
+    items: [],
+    addToCart: function (product) {
+        this.items.push(product);
+
+        this.totalCost = this.items.reduce((a, b) => a + parseFloat(b.price), 0)
+        document.querySelector("#total-price").innerText = this.totalCost;
+
+        const shoppingCartItemsDiv = document.querySelector("#shopping-cart-items");
+        shoppingCartItemsDiv.innerHTML = ""
+        this.items.forEach(item => {
+            shoppingCartItemsDiv.innerHTML += `
+                <li class="list-group-item d-flex justify-content-between" >
+                    <span>${item.name}</span> <span>${item.price}</span>
+                </li>
+            `
+        });
+
+        document.querySelector("#item-count").innerText = this.items.length
+    }
+}
+
+function endlistPOSProducts() {
+    fetch("http://localhost:3006/products")
+        .then(response => response.json())
+        .then(products => {
+
+            products.forEach(product => posEnlist(product));
+
+            const allAddToCartBtn = posPorductsDiv.querySelectorAll(".card button");
+
+            allAddToCartBtn.forEach((btn, i) => {
+                btn.addEventListener("click", () => {
+                    shoppingCart.addToCart(products[i])
+                })
+            });
+            
+        })
+}
+endlistPOSProducts()
